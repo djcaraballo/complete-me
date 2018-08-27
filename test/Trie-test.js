036bfc036e5330d876141d91c4ab2ba8fea60144
Dina <dcaraballo26@gmail.com>
2018-08-27 15:57:30 -0600

    Add insert describe block to trie test file

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -1,4 +1,38 @@
+import { assert } from 'chai';
+import { expect } from 'chai';
+import Trie from '../lib/Trie';
+
 //Trie should take an argument of a word
+
+describe('Trie', () => {
+  beforeEach(() => {
+    trie = new Trie()
+  });
+
+  it.skip('should be a function', () => {
+    assert.isFunction(Trie);
+  });
+
+  it.skip('should start with zero elements', () => {
+    expect(list.length).to.eq(0);
+  });
+
+  it.skip('should set its default head to null', () => {
+    expect(list.head).to.eq(null);
+  });
+
+});
+
+describe('INSERT', () => {
+  beforeEach(() => {
+    trie = new Trie()
+  });
+
+  it('should take a word as an argument', () => {
+
+  });
+
+});
 
 //insert
   //take in word
@@ -9,4 +43,3 @@
   //if node exists, or once node exists, shift off first letter in array
   //traverse to next letter
   //return to search for existing node as long as the length of the array is greater than zero
-  
