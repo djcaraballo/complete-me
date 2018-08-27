ebcd7f57344c4048c14bf559c4ef508fb594a93d
Dina <dcaraballo26@gmail.com>
2018-08-27 15:42:18 -0600

    Add pseudocode for first test

---
+++ test/Trie-test.js
@@ -0,0 +1,12 @@
+//Trie should take an argument of a word
+
+//insert
+  //take in word
+  //lowercase the word
+  //split word into letters array
+  //search for existing node
+  //if no node exists, create one
+  //if node exists, or once node exists, shift off first letter in array
+  //traverse to next letter
+  //return to search for existing node as long as the length of the array is greater than zero
+  
--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,13 @@
 import { assert } from 'chai';
+import Trie from "./lib/Trie";
+
     
  describe('test', function() {
    it('should return true', function() {
      assert.equal(true, true);
    });
  });
+
+ describe() {
+  it('should ')
+ }
